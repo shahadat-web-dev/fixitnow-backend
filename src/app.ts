2128996ba09225418f1be2df438b9c16762dd8f8
SHAHADAT - HOSSAIN <155247969+shahadat-web-dev@users.noreply.github.com>
2026-07-10 16:48:38 +0600

    Add middlewares Route notFound and globalErrorHandler

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,8 @@ import { bookingRoutes } from "./modules/booking/booking.route";
 import { paymentRoutes } from "./modules/payment/payment.route";
 import { adminRoutes } from "./modules/admin/admin.route";
 import { reviewRoutes } from "./modules/review/review.route";
+import { notFound } from "./middlewares/notFound";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
 
 
 const app: Application = express();
@@ -42,5 +44,10 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/reviews", reviewRoutes);
 
+
+
+app.use(notFound);
+
+app.use(globalErrorHandler);
 
 export default app;
